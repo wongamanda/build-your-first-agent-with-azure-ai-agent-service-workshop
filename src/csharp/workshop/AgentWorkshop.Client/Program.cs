bbab9a05312bb12ddb55a0c0f8ba02d4019adaff
Aaron Powell <me@aaron-powell.com>
2025-02-18 05:09:35 +0000

    Going back to the full cred stack

--- src/csharp/workshop/AgentWorkshop.Client/Program.cs
+++ src/csharp/workshop/AgentWorkshop.Client/Program.cs
@@ -11,7 +11,7 @@ var configuration = builder.Build();
 string apiDeploymentName = configuration["Azure:ModelName"] ?? throw new InvalidOperationException("MODEL_DEPLOYMENT_NAME is not set in the configuration.");
 string projectConnectionString = configuration.GetConnectionString("AiAgentService") ?? throw new InvalidOperationException("ConnectionStrings:AiAgentService is not set in the configuration.");
 
-AIProjectClient projectClient = new(projectConnectionString, new AzureCliCredential());
+AIProjectClient projectClient = new(projectConnectionString, new DefaultAzureCredential());
 
 await using Lab lab = new Lab3(projectClient, apiDeploymentName);
 
