c1d00cf9cd5bbb6ce3895cba41e8b3a20b790eab
Dave Glover <dglover@microsoft.com>
2025-04-30 13:44:14 +1000

    updated lab number

--- src/csharp/workshop/AgentWorkshop.Client/Lab5.cs
+++ src/csharp/workshop/AgentWorkshop.Client/Lab5.cs
@@ -2,7 +2,7 @@ using Azure.AI.Projects;
 
 namespace AgentWorkshop.Client;
 
-public class Lab4(AIProjectClient client, string modelName) : Lab(client, modelName)
+public class Lab5(AIProjectClient client, string modelName) : Lab(client, modelName)
 {
     protected override string InstructionsFileName => "code_interpreter_multilingual.txt";
 
